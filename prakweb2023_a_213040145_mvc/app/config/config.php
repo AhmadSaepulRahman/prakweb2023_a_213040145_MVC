8045752ac8d41bb788cd517d07ce335dc40793a8
AhmadSaepulRahman <96757821+AhmadSaepulRahman@users.noreply.github.com>
2023-10-23 21:55:32 +0700

    Add files via upload

---
+++ prakweb2023_a_213040145_mvc/app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://laragon/www/prakweb2023_a_213040145_mvc/public');
+
+// DB
+define('DB_HOST', 'laragon');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2023_a_213040145_mvc');
+
+
+?>
---
+++ prakweb2023_a_213040145_mvc/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model {
+    private $nama = "Ahmad Sr";
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
+
+?>
